d0b36d33d91b1b4a27bcd711a80a1b7c5e2e75d8
JesseLiFit <106931629+JesseLiFit@users.noreply.github.com>
2022-07-31 14:06:55 -0600

    Add files via upload

---
+++ WebScraper/getLoginToken.py
@@ -0,0 +1,17 @@
+import loginToken
+
+a = loginToken.test()
+
+token = a.seleniumLogin()
+
+print("I ran a new piece of code that gave me: ", token)
+
+
+
+# token = 'abcd1234'
+# headers = {
+#     'authority': 'api.trainerize.com',
+#     'test': token
+#   } 
+
+# print(headers)
