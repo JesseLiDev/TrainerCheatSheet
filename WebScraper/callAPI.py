1a9669f9bfb9c92535373a9ce0d3ad22f0e789da
JesseLiFit <jesselifitness@gmail.com>
2022-07-31 19:37:04 -0600

    Cleaned up code

--- WebScraper/callAPI.py
+++ WebScraper/callAPI.py
@@ -1,7 +1,9 @@
 from http import client
-import getLoginToken
-import getCurrentClients
-import getClientData
+import sys  
+sys.path.append(".")
+from getRequests.getLoginToken import getToken
+from getRequests.getCurrentClients import clientList
+from getRequests.getClientData import clientData
 
 #Get Login token
 # token = getLoginToken.getToken().seleniumLogin() 
@@ -10,17 +12,17 @@ import getClientData
 
  #get Current Clients
  
-clientList = getCurrentClients.clientList().getClientList() 
+clientList =  clientList().getClientList() 
 # print("I ran a new piece of code that gave me: ", clientList)
  
 
 #Pass each client in client list to
-
-
+#   Dependent on clientList
 a = 0
 for x in clientList:
-    clientData =[] 
-    clientData = getClientData.clientData().getClientData(clientList[a][0]) 
-    clientList[a].append(clientData)
+    clientStorage =[] 
+    clientStorage =  clientData().getClientData(clientList[a][0]) 
+    clientList[a].append(clientStorage)
     a += 1
 print("Test: ", clientList)
+
