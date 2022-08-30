6adc695ed2d376b0c6204c4191586921c82c4dc4
JesseLiFit <jesselifitness@gmail.com>
2022-08-30 13:46:57 -0600

    Overhaul Update

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -4,6 +4,10 @@ from .models import trainerLogin
 class trainerLoginForm(ModelForm):
     class Meta:
         model = trainerLogin
+        labels = {
+            "trainerUserName": "Trainerize Email Address",
+            "trainerPassword": "Trainerize Password"
+        }
         fields = '__all__'
 
         
